949a9768ad5cafddcea06d817d5d0803e63f4ba0
aschroed <andrew_schroeder@hms.harvard.edu>
2018-04-26 13:16:25 -0400

    get rid of pytest import

--- test/test_rxiv2ja.py
+++ test/test_rxiv2ja.py
@@ -1,4 +1,3 @@
-import pytest
 from wrangling import rxiv2ja as rj
 
 
@@ -42,3 +41,36 @@ def test_remove_skipped_vals_no_vals_to_skip():
     val = ['test_val1', 'test_val2', 'test_val3']
     result = rj.remove_skipped_vals(val)
     assert result == val
+
+
+def test_remove_skipped_vals_w_good_atids(mocker):
+    side_effect = ['uuid1', 'uuid2', 'uuid1', 'uuid2', 'uuid3']
+    val = ['id1', 'id2']
+    vals2skip = ['id1', 'id2', 'id3']
+    # import pdb; pdb.set_trace()
+    with mocker.patch('wrangling.script_utils.get_item_uuid',
+                      side_effect=side_effect):
+        result = rj.remove_skipped_vals(val, vals2skip)
+        assert not result
+
+
+def test_remove_skipped_vals_w_item_lookup(mocker):
+    side_effect = ['uuid1', 'uuid2', 'uuid1']
+    val = ['id1', 'id2']
+    vals2skip = ['id1']
+    # import pdb; pdb.set_trace()
+    with mocker.patch('wrangling.script_utils.get_item_uuid',
+                      side_effect=side_effect):
+        result = rj.remove_skipped_vals(val, vals2skip)
+        assert result[0] == val[1]
+
+
+def test_remove_skipped_vals_w_item_lookup_and_not_found(mocker):
+    side_effect = ['uuid1', None, 'uuid3', 'uuid1', None]
+    val = ['id1', 'id2', 'id3']
+    vals2skip = ['id3', 'id4']
+    # import pdb; pdb.set_trace()
+    with mocker.patch('wrangling.script_utils.get_item_uuid',
+                      side_effect=side_effect):
+        result = rj.remove_skipped_vals(val, vals2skip)
+        assert result[0] == val[0]
